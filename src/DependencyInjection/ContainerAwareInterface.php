5688241bb8349482bfaceef7667eaf06b3be405c
Alexandr <dubenko.alexandr@gmail.com>
2024-06-18 16:18:00 +0300

    Create ContainerAwareInterface.php

---
+++ src/DependencyInjection/ContainerAwareInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CacheTool\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+interface ContainerAwareInterface {
+  public function setContainer(?ContainerInterface $container);
+}
